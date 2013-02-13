92e997dd3c9c11c7d5fcebd9464ecfdef28ca947
Paul Moon <moon.haein@gmail.com>
2013-02-12 19:59:08 -0800

    Updated Problem 37 to use a dictionary instead of a list.

--- pe037.py
+++ pe037.py
@@ -1,26 +1,29 @@
-primes = [1]*10000000
-primes[0] = primes[1] = 0
+import math
+
+limit = 750000
+primes = {x: True for x in range(limit)}
+primes[0] = primes[1] = False
 solution = []
 
-
 # Sieve of Eratosthenes
-for i in range(2, 10000000):
-    if primes[i] == 1:
-        for j in range(2*i, 10000000, i):
-            primes[j] = 0
+for i in range(2, int(math.sqrt(limit)+1)):
+    if primes[i] == True:
+        for j in range(2*i, limit, i):
+            primes[j] = False
 
-for i in range(11, 10000000):
+for i in range(11, limit):
     is_truncatable = True
     temp = str(i)
     for j in range(len(temp)):
-        left = ''.join(list(temp)[:j+1])
-        right = ''.join(list(temp)[j:])
+        left = temp[:j+1]
+        right = temp[j:]
 
-        if primes[int(left)] == 0 or primes[int(right)] == 0:
+        if primes[int(left)] == False or primes[int(right)] == False:
             is_truncatable = False
+            break
     if is_truncatable:
         solution.append(i)
         if len(solution) == 11:
             break
 
-print sum(solution)
+print (sum(solution))
