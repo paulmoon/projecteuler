86f6dc225423216ffb68f0410b831203724a1211
Paul Moon <moon.haein@gmail.com>
2013-02-14 21:07:21 -0800

    Problem 9 solution added. ~0.035 seconds running time.

--- pe009.py
+++ pe009.py
@@ -1,9 +1,16 @@
 import math
 import sys
+import time
 
-for a in range(1, 999):
-    for b in range(1, 999):
-        c = math.sqrt(a**2 + b**2)
-        if a + b + c == 1000:
-            print(int(a*b*c))
-            sys.exit()
+start = time.clock()
+
+# In a Pythagoras triangle, a + b > c. If a + b + c = 1000, then it must be that c < 500,
+# otherwise a + b can never be greater than c since a + b = 1000 - c.
+# In addition, a < b < c implies that c > 334 since if c <= 334, then any choice of
+# b < c and a < b would give a + b + c < 1000.
+for c in range(334,500):
+    for a in range(1, (1000-c)//2):
+        b = 1000 - a - c
+        if a**2 + b**2 == c**2:
+            print (a*b*c)
+            print (time.clock() - start)
