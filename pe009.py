8028058948399857b9a9143d5c5dcdbee33b93be
Paul Moon <moon.haein@gmail.com>
2013-02-14 21:11:58 -0800

    Problem 9 solution added. ~0.035 seconds running time.

--- pe009.py
+++ pe009.py
@@ -1,13 +1,13 @@
-import math
-import sys
-import time
-
-start = time.clock()
-
 # In a Pythagoras triangle, a + b > c. If a + b + c = 1000, then it must be that c < 500,
 # otherwise a + b can never be greater than c since a + b = 1000 - c.
 # In addition, a < b < c implies that c > 334 since if c <= 334, then any choice of
 # b < c and a < b would give a + b + c < 1000.
+
+import math
+import time
+
+start = time.clock()
+
 for c in range(334,500):
     for a in range(1, (1000-c)//2):
         b = 1000 - a - c
