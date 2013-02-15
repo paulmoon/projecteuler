3b6b86c98cd54b040e59adf4aae2be10d7710b63
Paul Moon <moon.haein@gmail.com>
2013-02-15 09:12:06 -0800

    Problem 19 solution added. ~0.002 seconds running time.

--- pe019.py
+++ pe019.py
@@ -1,6 +1,11 @@
 import datetime
 import time
 
+def increment_month(dt_object):
+    month = dt_object.month % 12 + 1
+    year = dt_object.year + (month // 12)
+    return datetime.date(year, month, dt_object.day)
+
 start_time = time.clock()
 
 start = datetime.date(1901, 1, 1)
@@ -10,7 +15,7 @@ solution = 0
 while start <= end:
     if start.day == 1 and start.isoweekday() == 7:
         solution += 1
-    start += datetime.timedelta(days=1)
+    start = increment_month(start)
 
 print (solution)
 print (time.clock() - start_time)
