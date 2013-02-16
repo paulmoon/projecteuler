0730b786514fc70f8bab37dd03c9baadb83d2ba1
Paul Moon <moon.haein@gmail.com>
2013-02-15 18:14:54 -0800

    Problem 33 solution added. ~0.005 seconds running time.

--- pe033.py
+++ pe033.py
@@ -1,7 +1,9 @@
 import fractions
-import operator
+import time
 
-solution = []
+start = time.clock()
+num_product = 1
+den_product = 1
 
 for num in range(10, 100):
     for den in range(num, 100):
@@ -10,15 +12,15 @@ for num in range(10, 100):
             continue
         original = float(num)/den
         new = (float(str(num)[0]))
-        new_den = (int(str(den)[-1]))
+        new_den = (int(str(den)[1]))
         # Can't divide by 0
         if new_den == 0:
             continue
         new /= new_den
         if original == new:
-            solution.append((num, den))
+            num_product *= num
+            den_product *= den
 
-# zip(*) is unzipping
-num, den = zip(*solution)
-f = fractions.Fraction(reduce(operator.mul, num), reduce(operator.mul, den))
+f = fractions.Fraction(num_product, den_product)
 print (f.denominator)
+print (time.clock() - start)
